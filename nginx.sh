c04e463c04786a89d1a5baf0fb62bd625c5bb3bc
cashwin4dwin <asangoham42@gmail.com>
2022-10-07 19:58:36 -0700

    nginx changes

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf wanebu-backend-login.conf /etc/nginx/sites-available/wanebu-backend-login
+sudo cp -rf wanebu-backend-login.conf /etc/nginx/sites-available/login_ms
 chmod 777 /var/lib/jenkins/workspace/wanebu-backend-login-cicd
 
-sudo ln -sf /etc/nginx/sites-available/wanebu-backend-login /etc/nginx/sites-enabled
+sudo ln -sf /etc/nginx/sites-available/login_ms /etc/nginx/sites-enabled
 
 sudo nginx -t
 
