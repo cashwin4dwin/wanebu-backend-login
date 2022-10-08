647822838a19c2c115608a6f158126a0acdd032b
cashwin4dwin <asangoham42@gmail.com>
2022-10-07 20:20:01 -0700

    nginx changes

--- login_ms/login_ms/settings.py
+++ login_ms/login_ms/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-3$e6m*l6$xc!*3#4d3zgul60@s^o44(h-$hmrg3v)hp%akc^zd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
